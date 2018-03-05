5289cdac1e43f57343c6b65553569df12cd105e8
Tyler Tracy <tylertracy1999@gmail.com>
2018-03-05 13:16:40 -0600

    added some new functions and cleaned up some code. Still need to get things working more correctly

--- Checkers.py
+++ Checkers.py
@@ -18,21 +18,21 @@ pygame.init()
     
 # Set the width and height of the screen [width, height]
 size = (700, 700)
-squareLength = math.floor(size[0]/8);
+squareLength = math.floor(size[0]/8)
 screen = pygame.display.set_mode(size)
 pygame.display.set_caption("Checkers")    
 
 #creating the grid array
 grid = []
 for col in range(8):
-    grid.append([]);
+    grid.append([])
     for row in range(8):
         if row < 3 and (row+col)%2 == 0: #check if (row,col) is in the checker diagonal
-            grid[col].append("r"); #set the square to be red
+            grid[col].append("r") #set the square to be red
         elif row > 4 and (row+col)%2 == 0:
-            grid[col].append("b"); #set the square to be black
+            grid[col].append("b") #set the square to be black
         else:
-            grid[col].append("N"); #set the square to be empty
+            grid[col].append("N") #set the square to be empty
 
 clock = pygame.time.Clock() # Used to manage how fast the screen updates
 selected = (-1,-1) #this stores the value of the grid square that is currently selected
@@ -53,6 +53,23 @@ def otherColor(color):
 #function to save some typing
 def grd(t):
     return grid[t[0]][t[1]]
+
+
+def vailidJump(start,end): #check if I can jump a piece in this configuation
+    c = otherColor(grd(start))
+
+    if grd((start[0]+1,start[1]+1)) == c: #if it is next to a piece of the other color
+        return True
+    elif grd((start[0]-1,start[1]+1)) == c: #if it is next to a piece of the other color
+        return True
+    
+    if grd(start) == grd(start).upper(): #it is a king
+        #checking behind it
+        if grd((start[0]+1,start[1]-1)) == c: #if it is next to a piece of the other color
+            return True
+        elif grd((start[0]-1,start[1]-1)) == c: #if it is next to a piece of the other color
+            return True
+    return False
 
         
 #This function was written to not modify any data. 
@@ -87,7 +104,7 @@ def makeTurn(startSpot,endSpot):
         return [False,5,"Can't Jump your own Pieces"]
 
     #this is an array of values that will be replaced with "N", meaning we are deleting the piece in them
-    removeSpots = [];
+    removeSpots = []
     
     if (grd(endSpot) == "N"): #not jumping over piece
         dropSpot = endSpot
@@ -103,7 +120,15 @@ def makeTurn(startSpot,endSpot):
     removeSpots.append(startSpot)
     return [True,dropSpot,removeSpots]
         
-        
+def checkForJump():
+    for row in range(8):
+        for col in range(8):
+            #check all the boxes and the boxes around them
+            for i in range(-1,1,2):
+                for j in range(-1,1,2):
+                    if vailidJump((row,col),(row+i,col+j)):
+                        print("Good Jump")
+
 
 #=============================================
 #           Main Program Loop
@@ -115,17 +140,19 @@ while not done:
             done = True
     
     if pygame.mouse.get_pressed()[0] and not mousePressed: #mouse is down, for the first time
-        mousePressed = True;
+        mousePressed = True
         
-        pos = pygame.mouse.get_pos();
+        pos = pygame.mouse.get_pos()
         #check where click happened;
         clickedSquare = (math.floor(pos[0]/(size[0]/8)),math.floor(pos[1]/(size[1]/8)))
 
+
+        checkForJump()
         #logic for selecting thing
         if selected[0] == -1: #if nothing is selected
             
             if grd(clickedSquare).lower() == turn: #make sure it is your turn
-                selected = clickedSquare;
+                selected = clickedSquare
             elif grd(clickedSquare).lower() == otherColor(turn): #not your turn
                 print("Not your turn")
 
@@ -139,8 +166,8 @@ while not done:
             if res[0]: #if the move was sucsessful
                 
                 #set the new spot equal to the correct color
-                newSpot = res[1];
-                grid[newSpot[0]][newSpot[1]] = grd(selected);
+                newSpot = res[1]
+                grid[newSpot[0]][newSpot[1]] = grd(selected)
                 
                 #check if the newSquare is a king
                 if grd(newSpot) == "r" and newSpot[1] == 7: #if it is red and on the bottom row
@@ -166,7 +193,7 @@ while not done:
                 print(res[2])
                 
     elif pygame.mouse.get_pressed()[0] == 0: #mouse is up
-        mousePressed = False;
+        mousePressed = False
     
     #=================================================
     #              Drawing code
@@ -182,20 +209,20 @@ while not done:
     #draw the grid
     for i in range(0,8):
         for j in range(0,8):
-            squareX = i*squareLength;
-            squareY = j*squareLength;
+            squareX = i*squareLength
+            squareY = j*squareLength
             
             if selected[0] == i and selected[1] == j:
                 pygame.draw.rect(screen,green,[squareX,squareY,squareLength,squareLength])
             
             circleX = math.floor(i*squareLength+squareLength/2)
             circleY = math.floor(j*squareLength+squareLength/2)
-            radius = math.floor(size[0]/32);
+            radius = math.floor(size[0]/32)
             
             if grid[i][j].lower() == "r":
-                pygame.draw.circle(screen, red, [circleX, circleY], radius);
+                pygame.draw.circle(screen, red, [circleX, circleY], radius)
             elif grid[i][j].lower() == "b":
-                pygame.draw.circle(screen, black, [circleX, circleY], radius);
+                pygame.draw.circle(screen, black, [circleX, circleY], radius)
             
             #draw if it is a king
             if grid[i][j] == grid[i][j].upper() and grid[i][j] != "N":
