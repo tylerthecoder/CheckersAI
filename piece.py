0e2f6c2e1e1cacf43e1dfc514aa76704d59e7b1a
Tyler Tracy <tylertracy1999@gmail.com>
2018-03-18 22:02:27 -0500

    got everything working with classes, divied everything into different files.

---
+++ piece.py
@@ -0,0 +1,18 @@
+class Spot:
+    def __init__(self, color):
+        if color != "N": #it is a piece
+            self.isPlayer = True
+        else:
+            self.isPlayer = False
+        self.color = color
+        self.king = False
+    
+    def kingMe():
+        self.king = True
+
+    def setMe (self,spot):
+        self.color = spot.color
+        self.isPlayer = spot.isPlayer
+        self.king = spot.king
+
+    
